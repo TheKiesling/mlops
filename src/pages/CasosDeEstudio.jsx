29d6c36a98ddb9533e84f5d3739c7725bc40c0ea
TheKiesling <kie21581@uvg.edu.gt>
2025-08-08 17:30:45 -0600

    [feat]: components pages

---
+++ src/pages/CasosDeEstudio.jsx
@@ -0,0 +1,10 @@
+export default function CasosDeEstudio() {
+  return (
+    <div>
+      <h1>Casos de Estudio</h1>
+      <ul>
+        <li><a href="https://github.com/thekiesling/mlops/tree/main/casos-de-estudio">Casos de Estudio</a></li>
+      </ul>
+    </div>
+  );
+}
---
+++ src/pages/Proyectos.jsx
@@ -0,0 +1,10 @@
+export default function Proyectos() {
+  return (
+    <div>
+      <h1>Proyectos</h1>
+      <ul>
+        <li><a href="https://github.com/thekiesling/mlops/tree/main/proyectos">Proyectos</a></li>
+      </ul>
+    </div>
+  );
+}
